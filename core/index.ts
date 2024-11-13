7b4249042a50eee41347f27c2d5e6fff4831c67f
Yo <franjomor@gmail.com>
2024-11-13 10:37:18 +0100

    feat: initialization node ts server

---
+++ core/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => { 
+    console.log("Server running at PORT: ", PORT); 
+}).on("error", (error) => {
+    throw new Error(error.message);
+});
