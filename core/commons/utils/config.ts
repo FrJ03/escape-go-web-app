33ec3b2b9475c95af5ea168ef615041d2db71731
Yo <franjomor@gmail.com>
2024-12-06 11:09:22 +0100

    feat: salt in config

--- core/commons/utils/config.ts
+++ core/commons/utils/config.ts
@@ -2,17 +2,18 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const ENV = process.env.ENV
-const PORT = process.env.PORT
+const ENV = process.env.ENV || 'prod'
+const PORT = process.env.PORT || '3000'
 const POSTGRES_URL = 
     (ENV === 'prod') ? 
-        process.env.PROD_POSTGRES_URL
+        process.env.PROD_POSTGRES_URL || ''
     :
         (ENV === 'dev') ? 
-            process.env.DEV_POSTGRES_URL
+            process.env.DEV_POSTGRES_URL || ''
         :
-            process.env.TEST_POSTGRES_URL
+            process.env.TEST_POSTGRES_URL || ''
 
-const SSL = (ENV !== 'test')
+const SSL = (ENV !== 'test') || false
+const SALT = process.env.SALT || 10
 
-export { ENV, PORT, POSTGRES_URL, SSL }
+export { ENV, PORT, POSTGRES_URL, SSL, SALT }
