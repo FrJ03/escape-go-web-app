edcb3f05306fdae0cb297624193e6e66c316eff9
Yo <franjomor@gmail.com>
2024-12-16 16:30:45 +0100

    feat: game api register endpoint

---
+++ core/escape-room-sessions/infrastructure/api/game.api.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import { container } from "../../../commons/container/container";
+
+const GameRouter = express.Router();
+
+GameRouter.post('/register', async (req, res) => {
+    if(req.body.user_email === undefined || req.body.escape_room_id === undefined || req.body.participation_id === undefined){
+        res.sendStatus(400)
+    }
+
+    const request = {
+        user_email: req.body.user_email,
+        escape_room_id: req.body.escape_room_id,
+        participation_id: req.body.participation_id
+    }
+
+    const response = await container.registerParticipant.with(request)
+
+    res.sendStatus(response.code)
+})
