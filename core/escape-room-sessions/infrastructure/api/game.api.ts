3bc5b9c1615d2f927e857c9837d1c030914d3457
Yo <franjomor@gmail.com>
2024-12-17 12:23:40 +0100

    feat: get clue by id endpoint

--- core/escape-room-sessions/infrastructure/api/game.api.ts
+++ core/escape-room-sessions/infrastructure/api/game.api.ts
@@ -19,4 +19,28 @@ gameRouter.post('/register', async (req, res) => {
     res.sendStatus(response.code)
 })
 
+gameRouter.post('/clue/:id', async (req, res) => {
+    if(
+        !Number.isInteger(req.params.id) ||
+        req.body.escape_room_id === undefined ||
+        Number.isNaN(req.body.escape_room_id)
+    ){
+        res.sendStatus(400)
+    }
+
+    const request = {
+        clue_id: Number.parseInt(req.params.id),
+        escape_room_id: req.body.escape_room
+    }
+
+    const response = await container.getClue.with(request)
+
+    if(response.code !== 200){
+        res.sendStatus(response.code)
+    }
+    else{
+        res.send(response.clue).status(response.code)
+    }
+})
+
 export {gameRouter}
