59d64efa1082844647dec1e017a6afc97d7d431f
Yo <franjomor@gmail.com>
2024-12-22 13:16:40 +0100

    feat: get all user participatios service

--- core/game/domain/services/user-participations.repository.ts
+++ core/game/domain/services/user-participations.repository.ts
@@ -3,4 +3,5 @@ import { UserParticipation } from "../model/user-participation.entity";
 export interface UserParticipations{
     save(user_participation: UserParticipation): Promise<Boolean>
     getAllByParticipation(participation_id: number, escape_room_id: number): Promise<Array<UserParticipation>>
+    getAll(): Promise<Array<UserParticipation>>
 }
