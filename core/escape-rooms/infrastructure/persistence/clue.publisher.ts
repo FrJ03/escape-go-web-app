afc77a7c1c7e9a4966bd323f9f0ba28ce23a745e
Yo <franjomor@gmail.com>
2025-01-07 16:53:15 +0100

    fix: console log

--- core/escape-rooms/infrastructure/persistence/clue.publisher.ts
+++ core/escape-rooms/infrastructure/persistence/clue.publisher.ts
@@ -1,11 +1,4 @@
 import { Client, ClientConfig, Query, QueryResult } from "pg";
-import { Publisher } from "../../../commons/domain/publisher/publisher";
-import { EscapeRoom } from "../../domain/model/escapeRoom.entity";
-import { EscapeRoomDataMapper } from "./escape_room.data-mapper";
-import { INSERT_CITY, INSERT_COUNTRY, INSERT_ESCAPE_ROOM, INSERT_LOCATION, SELECT_CITY_BY_NAME, SELECT_COUNTRY_BY_NAME, SELECT_LOCATION } from "../queries/escape_rooms.query";
-import { CountryType } from "./country.type";
-import { CityType } from "./city.type";
-import PostgresSqlConfig from "../../../commons/infrastructure/database-client/postgresql-client";
 import { Clue } from "../../domain/model/clue.entity";
 import { ClueDataMapper } from "./clue.data-mapper";
 import { INSERT_CLUE } from "../queries/clues.query";
@@ -15,14 +8,18 @@ export class CluePublisher{
     
     async create(clue: Clue, escape_room_id: number): Promise<boolean>{
         const data = ClueDataMapper.toType(clue, escape_room_id)
+        console.log(data)
         try {
             const postgres = new Client(this.postgres_config)
             await postgres.connect()
             const response = await postgres.query(INSERT_CLUE, [data.escape_room, data.title, data.info])
             await postgres.end()
 
+            console.log(response.rowCount)
+
             return (response.rowCount !== 0)
         } catch (error) {
+            console.log(error)
             return false
         }
     }
